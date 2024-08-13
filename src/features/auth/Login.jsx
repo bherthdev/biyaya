6b4803d8616389ff9791331b97430b5b091f23a0
bherth <bherthchavez@gmail.com>
2024-08-13 21:52:52 +0800

    Update Login.jsx

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -9,6 +9,7 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import Switcher from "../../components/Switcher";
 import PageLoader from "../../components/PageLoader";
 import biyayaLogo from "../../assets/biyaya_logo.png";
+import biyayaShop1 from "../../assets/biyaya_shop.png";
 import { FaRegUser } from "react-icons/fa";
 import Spenner from "../../components/Spenner";
 import { motion as m } from "framer-motion"
@@ -66,181 +67,282 @@ const Login = () => {
   const handlePwdInput = (e) => setPassword(e.target.value);
   const handleToggle = () => setPersist((prev) => !prev);
 
-  const errClass = errMsg ? "list-disc mt-8 text-red-400 dark:text-red-300 text-xs tracking-wide" : null;
+  const errClass = errMsg ? "list-disc mt-8 text-red-600 dark:text-red-300 text-xs tracking-wide" : null;
 
   if (isLoading) return (
 
-    <div className="w-full h-screen flex flex-col items-center bg-biyaya bg-cover justify-center px-4">
-      <div ref={userRef} className="mt-7 overflow-x-auto border-gray-400  dark:border-gray-800  rounded-2xl overflow-hidden">
-        <div className="flex flex-col items-center  bg-gray-700 backdrop-blur-2xl bg-opacity-[20%] dark:bg-gray-900 px-10 py-8 sm:py-10 sm:px-28 ease-in-out duration-300">
+    <div className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <div ref={userRef} className="relative flex h-48 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt="Logo"
+            src={biyayaShop1}
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
 
-          <img src={biyayaLogo} className='animate-bounce shadow-md shadow-black  rounded-full h-[100px] xl:h-[150px] mx-auto ease-in-out duration-300' />
-          <div className="flex space-x-2">
-            <span className='mr-3 border-t-transparent border-solid animate-spin  rounded-full border-slate-100 border-2 h-6 w-6'></span>
+          <div className="hidden lg:relative lg:block lg:p-12">
+            <div className="block text-white">
+              <span className="sr-only">Home</span>
 
-            <div className='m-auto text-xs sm:text-lg text-white'>Logging in ...</div>
-          </div>
+              <img
+                alt="Logo"
+                src={biyayaLogo}
+                className="h-32 sm:h-32"
+              />
+            </div>
 
-          <div className="flex items-center gap-2 mt-16">
-            <p className="text-xs font-thin text-gray-100 dark:text-gray-400">Designed and developed by: </p>
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Welcome to Biyaya 🍵
+            </h2>
 
-            <a href='https://www.tyingknotworks.com/'
-              target='_blank' rel='noreferrer noopener'
-              className=" font-normal text-xs text-white"
-            >
-              TyingKnot Works
-            </a>
+            <p className="mt-4 leading-relaxed text-white/90">
+              Welcome to our cozy coffee haven! Whether you're here for your daily brew or a relaxing escape, we're delighted to serve you.
+            </p>
           </div>
         </div>
-      </div>
-    </div>
+
+        <div
+          className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+        >
+          <div className="flex flex-col  max-w-xl lg:max-w-3xl">
+            <div className="relative -mt-16 block lg:hidden">
+              <div
+                className="inline-flex size-28 items-center justify-center rounded-full bg-white sm:size-20"
+
+              >
+                <span className="sr-only">Home</span>
+                <img
+                  alt="Logo"
+                  src={biyayaLogo}
+
+                />
+              </div>
+
+              <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                Welcome to Biyaya 🍵
+              </h1>
+            </div>
+            <div className="flex flex-col items-center  bg-white dark:bg-gray-900 px-10 py-8 sm:py-10 sm:px-28 ease-in-out duration-300">
+
+              <img src={biyayaLogo} className='hidden sm:block animate-bounce shadow shadow-black  rounded-full h-[100px] xl:h-[100px] mx-auto ease-in-out duration-300' />
+              <div className="flex space-x-2 mt-20 sm:mt-0">
+                <span className='mr-3 border-t-transparent border-solid animate-spin  rounded-full border-slate-600 border-2 h-6 w-6'></span>
+
+                <div className='m-auto text-lg text-black'>Logging in ...</div>
+              </div>
+
+              <div className="flex items-center gap-2 mt-16">
+                <p className="text-xs font-thin text-gray-600 dark:text-gray-400">Designed and developed by: </p>
+
+                <a href='https:www.tyingknotworks.com/'
+                  target='_blank' rel='noreferrer noopener'
+                  className=" font-normal text-xs text-black"
+                >
+                  TyingKnot Works
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div >
+
+    </div >
+
 
   )
 
 
   const content = (
-    <div className="w-full h-screen flex flex-col items-center bg-biyaya bg-cover justify-center px-4">
-      <div className="mt-7 overflow-x-auto rounded-2xl overflow-hidden">
-        <div className=" bg-gray-700 backdrop-blur-2xl bg-opacity-[45%] dark:bg-gray-900 px-10 py-8 sm:py-10 sm:px-28 ease-in-out duration-300">
-          <div className="">
-            <m.img
-              initial={{ opacity: 0, filter: "blur(5px)" }}
-              animate={{ opacity: 1, filter: "blur(0px)" }}
-              transition={{ delay: 0.5, duration: 0.50, ease: "easeIn" }}
-              src={biyayaLogo}
-              className='h-[100px] xl:h-[130px] mx-auto ease-in-out duration-300' />
-          </div>
-          <m.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1, duration: 0.50, ease: "easeIn" }}
-            className="sm:w-72 text-black">
-            <div className="text-center">
-              <div className="mt-5 space-y-2">
+    <>
+      <div className="bg-white">
+        <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+          <div className="relative flex h-48 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+            <img
+              alt="Logo"
+              src={biyayaShop1}
+              className="absolute inset-0 h-full w-full object-cover opacity-80"
+            />
 
-                <h3 className="text-white dark:text-gray-300 text-xl font-bold tracking-widest font-sans">
-                  Login
-                </h3>
-                <p
-                  className="text-white dark:text-gray-300 text-sm font-thin text-left pt-5">
-                  Enter you login information to access the system.
-                </p>
-              </div>
+            <div className="hidden lg:relative lg:block lg:p-12">
+              <div className="block text-white">
+                <span className="sr-only">Home</span>
 
-              <ul className="list-inside">
-                <li ref={errRef} className={errClass} aria-live="assertive">
-                  {errMsg}
-                </li>
-              </ul>
-            </div>
-            <form onSubmit={handleSubmit} className="mt-8 space-y-5">
-
-              <div className="relative">
-                <label
-                  htmlFor="UserEmail"
-                  className="block overflow-hidden bg-white rounded-lg border border-gray-200 px-3 pb-2 pt-1"
-                >
-                  <span className="text-xs font-light tracking-wider text-gray-500"> Username: </span>
-
-                  <input
-                    type="text"
-                    id="username"
-                    ref={userRef}
-                    value={username}
-                    onChange={handleUserInput}
-                    autoComplete="current-password"
-                    required={true}
-                    className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-lg"
-                  />
-                  <div className="absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-400 text-lg">
-
-                    <FaRegUser size={20} className="mr-2" />
-
-                  </div>
-
-
-                </label>
-              </div>
-
-              <div className="relative">
-                <label
-                  htmlFor="UserEmail"
-                  className="block overflow-hidden bg-white rounded-lg  px-3 pb-2 pt-1 shadow-sm"
-                >
-                  <span className="text-xs font-light tracking-wider text-gray-500"> Password: </span>
-
-                  <input
-                    type={passwordShown ? "text" : "password"}
-                    id="password"
-                    onChange={handlePwdInput}
-                    value={password}
-                    autoComplete="off"
-                    required
-                    className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-lg"
-                  />
-                  <span className="absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-300 text-lg">
-
-                    <div className="absolute inset-y-0 right-0 flex items-center px-2">
-                      <input
-                        className="hidden js-password-toggle"
-                        id="toggle"
-                        type="checkbox"
-                        onClick={togglePasswordVisiblity}
-                      />
-                      <label
-                        className={`bg-white hover:bg-slate-100 dark:bg-slate-900 dark:hover:bg-slate-800  rounded-xl px-2 py-1 mt-1 text-lg text-gray-400 dark:text-gray-700 font-mono cursor-pointer js-password-label`}
-                        htmlFor="toggle"
-                      >
-                        {passwordShown ? (
-                          <AiOutlineEye size={20} />
-                        ) : (
-                          <AiOutlineEyeInvisible size={20} />
-                        )}
-                      </label>
-                    </div>
-                  </span>
-
-
-                </label>
-              </div>
-              <div
-                className="pb-3">
-                <input
-                  type="checkbox"
-                  className="accent-slate-200"
-                  id="persist"
-                  onChange={handleToggle}
-                  checked={persist}
+                <img
+                  alt="Logo"
+                  src={biyayaLogo}
+                  className="h-32 sm:h-32"
                 />
-                <label htmlFor="persist" className="pl-2 text-white font-thin tracking-wide">
-                  Trust this device
-                </label>
               </div>
 
-              <button
-                className="w-full px-4 py-3 font-sans text-white bg-[#1E1E1E] hover:bg-slate-700 active:bg-slate-800 rounded-full duration-150">
-                LOGIN
-              </button>
-            </form>
-          </m.div>
-        </div>
-      </div>
+              <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+                Welcome to Biyaya 🍵
+              </h2>
 
-      <m.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5, duration: 0.75, ease: "easeIn" }}
-        className="flex items-center gap-2 mt-5">
-        <p className="text-xs font-thin text-gray-300 dark:text-gray-400">Designed and developed by: </p>
+              <p className="mt-4 leading-relaxed text-white/90">
+                Welcome to our cozy coffee haven! Whether you're here for your daily brew or a relaxing escape, we're delighted to serve you.
+              </p>
+            </div>
+          </div>
 
-        <a href='https://www.tyingknotworks.com/'
-          target='_blank' rel='noreferrer noopener'
-          className=" font-normal text-xs text-gray-300"
-        >
-          TyingKnot Works
-        </a>
-      </m.div>
-    </div>
+          <div
+            className="flex  items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+          >
+            <div className="flex flex-col  max-w-xl lg:max-w-3xl">
+              <div className="relative -mt-16 block lg:hidden">
+                <div
+                  className="inline-flex size-28 items-center justify-center rounded-full bg-white sm:size-20"
+
+                >
+                  <span className="sr-only">Home</span>
+                  <img
+                    alt="Logo"
+                    src={biyayaLogo}
+
+                  />
+                </div>
+
+                <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                  Welcome to Biyaya 🍵
+                </h1>
+
+                {/* <p className="mt-4 leading-relaxed text-gray-500">
+              Welcome to our cozy coffee haven! Whether you're here for your daily brew or a relaxing escape, we're delighted to serve you.
+
+            </p> */}
+              </div>
+              <form onSubmit={handleSubmit} className=" space-y-7">
+                <div className=" space-y-2 sm:text-center">
+
+                  <h3 className="text-black hidden sm:block dark:text-gray-300 text-xl font-bold tracking-widest font-sans">
+                    LOGIN
+                  </h3>
+                  <p
+                    className="text-black dark:text-gray-300 text-base font-thin text-left pt-5">
+                    Enter you login information to access the system.
+                  </p>
+                </div>
+
+                <ul className="list-inside">
+                  <li ref={errRef} className={errClass} aria-live="assertive">
+                    {errMsg}
+                  </li>
+                </ul>
+
+                <div className="relative">
+                  <label
+                    htmlFor="UserEmail"
+                    className="block overflow-hidden bg-white rounded-lg border-2 px-3 pb-2 pt-1"
+                  >
+                    <span className="text-xs font-light tracking-wider text-gray-700"> Username: </span>
+
+                    <input
+                      type="text"
+                      id="username"
+                      ref={userRef}
+                      value={username}
+                      onChange={handleUserInput}
+                      autoComplete="current-password"
+                      required={true}
+                      className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-lg"
+                    />
+                    <div className="absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-400 text-lg">
+
+                      <FaRegUser size={20} className="mr-2" />
+
+                    </div>
+
+
+                  </label>
+                </div>
+
+                <div className="relative">
+                  <label
+                    htmlFor="UserEmail"
+                    className="block overflow-hidden bg-white rounded-lg  px-3 pb-2 pt-1 border-2"
+                  >
+                    <span className="text-xs font-light tracking-wider text-gray-700"> Password: </span>
+
+                    <input
+                      type={passwordShown ? "text" : "password"}
+                      id="password"
+                      onChange={handlePwdInput}
+                      value={password}
+                      autoComplete="off"
+                      required
+                      className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-lg"
+                    />
+                    <span className="absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-300 text-lg">
+
+                      <div className="absolute inset-y-0 right-0 flex items-center px-2">
+                        <input
+                          className="hidden js-password-toggle"
+                          id="toggle"
+                          type="checkbox"
+                          onClick={togglePasswordVisiblity}
+                        />
+                        <label
+                          className={`bg-white hover:bg-slate-100 dark:bg-slate-900 dark:hover:bg-slate-800  rounded-xl px-2 py-1 mt-1 text-lg text-gray-400 dark:text-gray-700 font-mono cursor-pointer js-password-label`}
+                          htmlFor="toggle"
+                        >
+                          {passwordShown ? (
+                            <AiOutlineEye size={20} />
+                          ) : (
+                            <AiOutlineEyeInvisible size={20} />
+                          )}
+                        </label>
+                      </div>
+                    </span>
+
+
+                  </label>
+                </div>
+                <div
+                  className="pb-3">
+                  <input
+                    type="checkbox"
+                    className="accent-slate-200"
+                    id="persist"
+                    onChange={handleToggle}
+                    checked={persist}
+                  />
+                  <label htmlFor="persist" className="pl-2 text-black font-thin tracking-wide">
+                    Trust this device
+                  </label>
+                </div>
+
+                <button
+                  className="w-full px-4 py-3  font-sans text-white bg-[#1E1E1E] hover:bg-slate-700 active:bg-slate-800 rounded-full duration-150">
+                  LOGIN
+                </button>
+
+
+              </form>
+
+              <m.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1.5, duration: 0.75, ease: "easeIn" }}
+                className="flex  justify-center  gap-2 mt-20">
+                <p className="text-xs font-thin text-gray-500 dark:text-gray-400">Designed and developed by: </p>
+
+                <a href='https:www.tyingknotworks.com/'
+                  target='_blank' rel='noreferrer noopener'
+                  className=" font-normal text-xs text-gray-600"
+                >
+                  TyingKnot Works
+                </a>
+              </m.div>
+            </div>
+
+          </div>
+        </div >
+      </div >
+
+
+    </>
+
+
 
   );
   return content;
